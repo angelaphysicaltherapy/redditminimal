49457489790ab355b5ff4c1d36147e166b98c838
anqizhang <angela.cheung1026@gmail.com>
2022-04-25 11:00:14 -0500

    fetch memes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import './App.css';
+import React, {useState} from 'react';
 
+var after = '';
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const getPost = ()=>{
+    fetch(`https://www.reddit.com/r/memes.json?after=${after}`)
+    .then(response=>response.json())
+    .then(body=>{
+      after= body.data.after;
+      setPosts(body.data.children);
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Welcome to a minimal version of Reddit!
         </p>
-        <button>Refrech</button>
-        
+        <button onClick={getPost}>Refrech</button>
       </header>
+      <div id="meme-container">
+        {
+          posts.map(post=>{
+            if(post.data.post_hint ==='image'){
+              return (
+                <div>
+                  <h4>{post.data.title}</h4>
+                  <img src={post.data.url_overridden_by_dest} />
+                </div>
+              )
+            }
+            
+          })
+        }
+    
+      </div>
     </div>
   );
 }
