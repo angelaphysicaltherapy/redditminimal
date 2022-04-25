c23f6754b9cbc3bfffd4f54fe63fcc1f16bbd451
anqizhang <angela.cheung1026@gmail.com>
2022-04-25 17:02:34 -0500

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -15,12 +15,21 @@ function App() {
   }
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to a minimal version of Reddit!
-        </p>
-        <button onClick={getPost}>Refrech</button>
+      {/* <header className="App-header"> */}
+      <header>
+        <div class="container-fluid">
+        <img src="https://picsum.photos/id/256/2000/300" class="img-fluid" alt="..."/>
+        </div>
       </header>
+      <div class="my-5">
+        <h1 >
+          Welcome to a minimal version of Reddit!
+        </h1>
+      </div>
+        
+        <div class="d-grid gap-2 col-4 mx-auto">
+            <button class="btn btn-primary btn-lg" type="button"onClick={getPost}>Refrech</button>
+        </div>
       <div id="meme-container">
         {
           posts.map(post=>{
